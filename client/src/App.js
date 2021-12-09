eb87821ab27c02a05f483d04f19603759db17a7d
Natalia Davydova <dav.nata@mail.ru>
2021-12-09 18:02:25 +0500

    add Sidebar component

--- client/src/App.js
+++ client/src/App.js
@@ -1,15 +1,16 @@
 import React from 'react';
+import { GlobalStyle } from './Components/styles/GlobalStyle';
 import { NewOffersComponent } from './Components/NewOffersComponent';
 import { HotOffersComponent } from './Components/HotOffersComponent';
 import { ExpiringOffersComponent } from './Components/ExpiringOffersComponent';
+import Sidebar from './Components/Sidebar';
 
 function App() {
   return (
-    <>
-      <NewOffersComponent />
-      <HotOffersComponent />
-      <ExpiringOffersComponent />
-    </>
+    <React.Fragment>
+      <GlobalStyle />
+      <Sidebar />
+    </React.Fragment>
   );
 }
 
